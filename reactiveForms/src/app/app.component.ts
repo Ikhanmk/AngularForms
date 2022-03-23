ca7a9f671f4c1ffe0694990bd992bc6cea1b14f2
ikhanmk <ikhanmk@gmail.com>
2022-03-23 13:01:30 +0530

    Loading data from api to forms

--- reactiveForms/src/app/app.component.ts
+++ reactiveForms/src/app/app.component.ts
@@ -17,4 +17,26 @@ export class AppComponent {
       postalCode: new FormControl('')
     })
   })
+
+  // loadApiData() {
+  //   this.registrationForm.setValue({---> setValue is used when we have to set values to all the form fields, if u skip a field it gives error
+  //     userName: "Bruce",
+  //     password : "test",
+  //     confirmPassword: "test",
+  //     address: {
+  //       city: 'city',
+  //       state: 'state',
+  //       postalCode: "67575758"
+  //     }
+  //   })
+  // }
+
+  loadApiData() {
+      this.registrationForm.patchValue({ 
+        userName: "Bruce",
+        password : "test",
+        confirmPassword: "test",
+       /*patch value can be used to fill up few of the fields  */ 
+      })
+    }
 }
